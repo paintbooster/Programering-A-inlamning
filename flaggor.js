1945ddd0bb3bf1f1180925dd00b7cddf06df2831
Linus <cr4wl3r_pro@hotmail.com>
2012-05-02 11:06:47 +0200

    Start

---
+++ flaggor.js
@@ -0,0 +1,18 @@
+var draw = startCanvas("maincanvas");
+
+draw.fillRect(100, 50, 200, 100, "blue");
+draw.fillRect(160, 50, 25, 100, "yellow");
+draw.fillRect(100, 87.5, 200, 25, "yellow");
+
+
+draw.fillRect(400, 50, 200, 100, "red");
+draw.fillRect(460, 50, 25, 100, "white");
+draw.fillRect(400, 87.5, 200, 25, "white");
+
+
+draw.fillRect(750, 50, 200, 100, "white");
+draw.fillRect(810, 50, 25, 100, "blue");
+draw.fillRect(750, 87.5, 200, 25, "blue");
+
+draw.fillRect(75, 40, 25, 410, "white");
+draw.circle(87, 30, 20, "gold");
